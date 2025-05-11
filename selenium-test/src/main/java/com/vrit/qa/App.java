b3e5453150726155cc760b171702ab6c6fece7de
Subash Singh Dhami <subashdhamee@gmail.com>
2025-05-11 11:44:34 +0545

    initial commit

---
+++ selenium-test/src/main/java/com/vrit/qa/App.java
@@ -0,0 +1,13 @@
+package com.vrit.qa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
